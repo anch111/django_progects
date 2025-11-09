79b5a602b7f6263013598006d9f7ffb0cc02b1db
Anna_Zenkina <zenkina.21@yandex.ru>
2025-11-09 19:37:24 +0500

    Update urls.py

--- starwars/peple/urls.py
+++ starwars/peple/urls.py
@@ -2,8 +2,9 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('luke_skywalker/', views.get_luke_info).
+    path('luke_skywalker/', views.get_luke_info)
 ]
+
 
 
 
