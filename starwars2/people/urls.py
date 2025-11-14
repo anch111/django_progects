dfef6bc146ea22daf24dba1ed163fe2d09a1bb57
Anna_Zenkina <zenkina.21@yandex.ru>
2025-11-14 18:43:33 +0500

    Update urls.py

--- starwars2/people/urls.py
+++ starwars2/people/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
-.
+
 urlpatterns = [
     path('luke_skywalker/', views.get_luke_data), 
     path('', views.get_people_list, name='people_list'),
     path('character/<int:id>', views.get_character_data, name='character')
 ]
+
 
 
 
